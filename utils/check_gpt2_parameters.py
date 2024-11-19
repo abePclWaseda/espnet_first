d92f9eb62c457a3f1865ade15e892c6adc2a01a1
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2024-11-19 17:09:02 +0900

    check_gpt2_parameters.py追加

---
+++ utils/check_gpt2_parameters.py
@@ -0,0 +1,27 @@
+#  /mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/lm_train_transformer_gpt2_en_hugging_face/gpt2_pretrained.pth に入っているgpt2のパラメータを確認する.
+#  gpt2で言語モデルを初期化できてない可能性あり,,,.
+#  deepcopyメソッドが機能してるかわからない. (optだとこれでできていたが.)
+import torch
+
+# モデルファイルのパス
+model_file = "/mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/lm_train_transformer_gpt2_en_hugging_face/gpt2_pretrained.pth"
+
+# state_dictのロード
+state_dict = torch.load(model_file, map_location="cpu")
+
+# クロスアテンション層のキーが存在するか確認
+# cross_attention_keys = [k for k in state_dict.keys() if "crossattention" in k]
+# print(f"クロスアテンション層のパラメータ数: {len(cross_attention_keys)}")
+# print(f"クロスアテンション層のキー: {cross_attention_keys}")
+# print(state_dict.keys())
+
+# 必要に応じて特定のパラメータ値を表示（最初の10個の値を表示する例）
+# if cross_attention_keys:
+#     for key in cross_attention_keys:
+#         print(f"{key}: {state_dict[key].flatten()[:10]}") 
+
+specific_key = "h.0.attn.c_attn.weight"
+if specific_key in state_dict.keys():
+    print(f"{specific_key}: {state_dict[specific_key].flatten()[0:10]}")
+else:
+    print(f"{specific_key} はキーに含まれていません。")
