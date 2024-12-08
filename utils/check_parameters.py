9967eb3d37420bac0010fbf620b07abe68f33f16
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2024-12-08 14:58:35 +0900

    check_parameters 変更

--- utils/check_parameters.py
+++ utils/check_parameters.py
@@ -2,8 +2,9 @@ import torch
 
 # モデルファイルのパス
 # model_file = "egs2/librispeech_100/asr1/exp/asr_train_asr_conformer_lr2e-3_warmup15k_amp_nondeterministic_raw_en_hugging_face_openai-community-gpt2_sp_1117GPT2Decoder/valid.acc.ave.pth"
-# model_file = "/mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/lm_train_transformer_gpt2_en_hugging_face/gpt2_pretrained.pth" # gpt2のパラメータがロードできているかの確認に使える.
-model_file = "/mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/asr_train_asr_conformer_lr2e-3_warmup15k_amp_nondeterministic_raw_en_hugging_face_openai-community-gpt2_sp/69epoch.pth"
+model_file = "/mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/lm_train_transformer_gpt2_en_hugging_face/gpt2_pretrained.pth" # gpt2のパラメータがロードできているかの確認に使える.
+# model_file = "/mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/asr_train_asr_conformer_lr2e-3_warmup15k_amp_nondeterministic_raw_en_hugging_face_openai-community-gpt2_sp/69epoch.pth"
+# model_file = "/mnt/kiso-qnap/abe/b4/espnet/egs2/librispeech_100/asr1/exp/asr_train_asr_conformer_lr2e-3_warmup15k_amp_nondeterministic_raw_en_hugging_face_openai-community-gpt2_sp/valid.acc.ave.pth"
 
 # state_dictのロード
 state_dict = torch.load(model_file, map_location="cpu")
@@ -24,8 +25,8 @@ decoder_keys = [key for key in state_dict.keys() if key.startswith("decoder.deco
 #     for key in cross_attention_keys:
 #         print(f"{key}: {state_dict[key].flatten()[:10]}") 
 
-specific_key = "decoder.decoder.h.0.attn.c_attn.weight"
-# specific_key = "lm.decoder.h.0.attn.c_attn.weight"
+# specific_key = "decoder.decoder.h.0.attn.c_attn.weight"
+specific_key = "lm.decoder.h.0.attn.c_attn.weight"
 if specific_key in state_dict.keys():
     print(f"{specific_key}: {state_dict[specific_key].flatten()[0:10]}")
 else:
