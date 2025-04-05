1f5e5a1a3a45985e213aee34a9edfdbd3439eedc
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-04-06 01:06:59 +0900

    whisperのfine-tuning(LoRAでもできない)

--- egs2/librispeech_100/asr1/run.sh
+++ egs2/librispeech_100/asr1/run.sh
@@ -13,9 +13,13 @@ asr_config=conf/tuning/train_asr_whisper_full.yaml
 inference_config=conf/tuning/decode_asr_whisper_noctc_greedy.yaml
 
 ./asr.sh \
+    --token_type whisper_multilingual \
+    --asr_speech_fold_length 256 \
+    --asr_text_fold_length 150 \
+    --cleaner whisper_basic \
     --lang en \
     --ngpu 1 \
-    --nj 8 \
+    --nj 1 \
     --gpu_inference true \
     --inference_nj 2 \
     --nbpe 5000 \
